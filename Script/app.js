ccce8c8719fb8dc7d38df88ed2be7a152eadd57f
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-05 17:15:32 +0600

    all tools fetched

--- Script/app.js
+++ Script/app.js
@@ -1,1 +1,22 @@
-console.log('connected');
+const LoadAlldata = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+    const data = await res.json();
+    const alldata = data.data.tools;
+    console.log(alldata);
+    showInDisplay();
+
+}
+
+const showInDisplay = () => {
+    const universContainer = document.getElementById('universe-container');
+    universContainer.innerHTML =
+        `
+     <h1 class="text-2xl font-bold">HI</h1>
+    `
+}
+
+
+
+
+//=====RUNTIME Exicute===========================?
+LoadAlldata();
